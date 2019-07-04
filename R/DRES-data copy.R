14fa6d9fea14cc0a92a7bac4b20869db66fffc57
John K Sakaluk <jksakaluk@gmail.com>
2019-07-04 14:16:25 -0400

    Add DRES data set (Raposo & Muise, under review) as an example data set.

---
+++ R/DRES-data copy.R
@@ -0,0 +1,22 @@
+#' Relationship quality and sexual satisfaction of 121 couples
+#'
+#'A dataset containing 9 observed indicators of relationship quality (PRQC)
+#'and 5 observed indicators of sexual satisfaction from 121 couples
+#'in the DRES (Daily Relationship Experiences Study; Raposo & Muise, under review)
+#'
+#' @docType data
+#'
+#' @usage data(DRES)
+#'
+#' @format A data frame with 121 rows and 28 variables:
+#' \describe{
+#'  \item{PRQC_1.1}{PRQC items for partner 1}
+#'  \item{PRQC_1.2}{PRQC items for partner 2}
+#'  \item{sexsat1.1}{sexual satisfaction items for partner 1}
+#'  \item{sexsat1.1}{sexual satisfaction items for partner 2}
+#' }
+#'
+#'@references Raposo, S., & Muise, A. (Under review). Perceived Partner Sexual Responsiveness Buffers Anxiously Attached Individuals from Lower Relationship and Sexual Quality in Daily Life. Social Psychological and Personality Science.
+#'
+#'
+"DRES"
