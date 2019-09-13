97beb756357b5569839193b382aa2396c7a9caae
John K Sakaluk <jksakaluk@gmail.com>
2019-09-13 12:12:35 -0700

    Add robust argument to enable outputting robust estimator fit indexes and comparisons

--- R/dyMIOutput.R
+++ R/dyMIOutput.R
@@ -6,6 +6,7 @@
 #' @param config fitted dyadic configural invariance model lavaan object
 #' @param load fitted dyadic loading invariance model lavaan object
 #' @param int fitted dyadic intercept invariance model lavaan object
+#' @param robust logical for whether a robust estimator has been used (default is F)
 #' @return Prints word files for the table outputed to the users working directory
 #' @seealso \code{\link{dyadCFA}} which this function relies on
 #' @export
@@ -21,9 +22,9 @@
 #' con.fit.loading <- cfa(con.loading.script, data = dat, std.lv = F, auto.fix.first= F, meanstructure = T)
 #' con.fit.intercept <- cfa(con.intercept.script, data = dat, std.lv = F, auto.fix.first= F, meanstructure = T)
 #'
-#' dyMIOutput(con.fit.config, con.fit.loading, con.fit.intercept)
+#' dyMIOutput(con.fit.config, con.fit.loading, con.fit.intercept, robust = F)
 
-dyMIOutput <-function(config, load, int){
+dyMIOutput <-function(config, load, int, robust = F){
   dirs("output")
   dirs("output/tables")
   #Extract and transpose fit indices from each model
@@ -39,32 +40,60 @@ dyMIOutput <-function(config, load, int){
   #Combine transposed fit indices for each model
   fit <- rbind(config.fit.t, loading.fit.t, int.fit.t)
 
-  #Extract vectors of fit indices of interest
-  Model <-c("Configural", "Loading", "Intercept")
-  chi2 <-fit$chisq
-  df <-fit$df
-  AIC <-fit$aic
-  BIC <-fit$bic
-  CFI <-fit$cfi
-  RMSEA <-fit$rmsea
-  RMSEA_LL <-fit$rmsea.ci.lower
-  RMSEA_UL <-fit$rmsea.ci.upper
+  if(robust ==F){
+    #Extract vectors of fit indices of interest
+    Model <-c("Configural", "Loading", "Intercept")
+    chi2 <-fit$chisq
+    df <-fit$df
+    AIC <-fit$aic
+    BIC <-fit$bic
+    CFI <-fit$cfi
+    RMSEA <-fit$rmsea
+    RMSEA_LL <-fit$rmsea.ci.lower
+    RMSEA_UL <-fit$rmsea.ci.upper
 
-  #Assemble table of descriptive indices
-  fit.table <-data.frame(Model, chi2, df, AIC, BIC, CFI, RMSEA, RMSEA_LL, RMSEA_UL)
+    #Assemble table of descriptive indices
+    fit.table <-data.frame(Model, chi2, df, AIC, BIC, CFI, RMSEA, RMSEA_LL, RMSEA_UL)
 
-  #Calculate vectors of comparison indices
-  chi2_diff <-c(NA, tail(fit.table$chi2, -1) - head(fit.table$chi2, -1))
-  df_diff <-c(NA, tail(fit.table$df, -1) - head(fit.table$df, -1))
-  p <-c(pchisq(chi2_diff,df_diff, lower.tail = FALSE))
-  AIC_diff <-c(NA, tail(fit.table$AIC, -1) - head(fit.table$AIC, -1))
-  BIC_diff <-c(NA, tail(fit.table$BIC, -1) - head(fit.table$BIC, -1))
-  CFI_diff <-c(NA, tail(fit.table$CFI, -1) - head(fit.table$CFI, -1))
-  RMSEA_diff <-c(NA, tail(fit.table$RMSEA, -1) - head(fit.table$RMSEA, -1))
+    #Calculate vectors of comparison indices
+    chi2_diff <-c(NA, tail(fit.table$chi2, -1) - head(fit.table$chi2, -1))
+    df_diff <-c(NA, tail(fit.table$df, -1) - head(fit.table$df, -1))
+    p <-c(pchisq(chi2_diff,df_diff, lower.tail = FALSE))
+    AIC_diff <-c(NA, tail(fit.table$AIC, -1) - head(fit.table$AIC, -1))
+    BIC_diff <-c(NA, tail(fit.table$BIC, -1) - head(fit.table$BIC, -1))
+    CFI_diff <-c(NA, tail(fit.table$CFI, -1) - head(fit.table$CFI, -1))
+    RMSEA_diff <-c(NA, tail(fit.table$RMSEA, -1) - head(fit.table$RMSEA, -1))
 
-  #Assemble table of comparison indices
-  comp.table <-data.frame(Model, chi2_diff, df_diff, p, AIC_diff, BIC_diff, CFI_diff, RMSEA_diff)
+    #Assemble table of comparison indices
+    comp.table <-data.frame(Model, chi2_diff, df_diff, p, AIC_diff, BIC_diff, CFI_diff, RMSEA_diff)
+  }
+  if(robust == T){
+    #Extract vectors of fit indices of interest
+    Model <-c("Configural", "Loading", "Intercept")
+    chi2 <-fit$chisq.scaled
+    df <-fit$df.scaled
+    AIC <-fit$aic
+    BIC <-fit$bic
+    CFI <-fit$cfi.robust
+    RMSEA <-fit$rmsea.scaled
+    RMSEA_LL <-fit$rmsea.ci.lower.scaled
+    RMSEA_UL <-fit$rmsea.ci.upper.scaled
 
+    #Assemble table of descriptive indices
+    fit.table <-data.frame(Model, chi2, df, AIC, BIC, CFI, RMSEA, RMSEA_LL, RMSEA_UL)
+
+    #Calculate vectors of comparison indices
+    chi2_diff <-c(NA, tail(fit.table$chi2, -1) - head(fit.table$chi2, -1))
+    df_diff <-c(NA, tail(fit.table$df, -1) - head(fit.table$df, -1))
+    p <-c(pchisq(chi2_diff,df_diff, lower.tail = FALSE))
+    AIC_diff <-c(NA, tail(fit.table$AIC, -1) - head(fit.table$AIC, -1))
+    BIC_diff <-c(NA, tail(fit.table$BIC, -1) - head(fit.table$BIC, -1))
+    CFI_diff <-c(NA, tail(fit.table$CFI, -1) - head(fit.table$CFI, -1))
+    RMSEA_diff <-c(NA, tail(fit.table$RMSEA, -1) - head(fit.table$RMSEA, -1))
+
+    #Assemble table of comparison indices
+    comp.table <-data.frame(Model, chi2_diff, df_diff, p, AIC_diff, BIC_diff, CFI_diff, RMSEA_diff)
+  }
   #Round numeric vectors in tables
   fit.table = fit.table %>%
     dplyr::mutate_if(is.numeric, round, digits = 3)
