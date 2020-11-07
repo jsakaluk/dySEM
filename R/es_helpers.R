e5bc07b362653b8839c759bc31e57fdd40b99e7b
John K. Sakaluk <jksakaluk@gmail.com>
2020-11-07 14:04:30 -0500

    Add improved dmacs calculating function (and helper functions) inspired by Gunn et al. (2020)

---
+++ R/es_helpers.R
@@ -0,0 +1,83 @@
+#' @name es_helpers
+#' @rdname es_helpers
+#'
+#' @title Helper functions for noninvariance effect size functions
+#'
+#' @param dat data frame of indicators
+#' @param fit outputted from dyadic cfa lavaan object
+#' @param source character for whether parameter of interest should be extracted for group "1", "2", or from the "nogroup" model
+#'
+#' @export
+
+#' @rdname es_helpers
+grouploads <- function(fit, source){
+  if(source == "1"){
+    loads <- dplyr::filter(lavaan::parameterEstimates(fit), op == "=~" & rhs %in% dvn[[1]]) %>%
+      dplyr::select(., est)
+  }else if(source == "2"){
+    loads <- dplyr::filter(lavaan::parameterEstimates(fit), op == "=~" & rhs %in% dvn[[2]]) %>%
+      dplyr::select(., est)
+  }
+  return(loads)
+}
+
+#' @rdname es_helpers
+groupints <- function(fit, source){
+  if(source == "1"){
+    ints <- lavaan::parameterEstimates(fit) %>%
+      dplyr::filter(., op == "~1" & lhs %in% dvn[[1]]) %>%
+      dplyr::select(., est)
+  }else if(source == "2"){
+    ints <- lavaan::parameterEstimates(fit) %>%
+      dplyr::filter(., op == "~1" & lhs %in% dvn[[2]]) %>%
+      dplyr::select(., est)
+  }
+  return(ints)
+}
+
+#' @rdname es_helpers
+groupindsds <- function(dat, source){
+
+  if(source == 1){
+    sds <- dat %>% dplyr::select(., paste(dvn[[1]])) %>%
+      dplyr::summarise_all(., sd, na.rm = TRUE) %>%
+      as.numeric(.)
+  }else if(source == 2){
+    sds <- dat %>% dplyr::select(., paste(dvn[[2]])) %>%
+      dplyr::summarise_all(., sd, na.rm = TRUE) %>%
+      as.numeric(.)
+  }
+
+  return(sds)
+}
+
+#' @rdname es_helpers
+grouplvmean <- function(fit, source){
+  if(source == "1"){
+    lvmean <- dplyr::filter(lavaan::parameterEstimates(fit), op == "~1" & lhs == lavaan::lavNames(fit, type = "lv")[[1]]) %>%
+      dplyr::select(., est) %>%
+      as.numeric(.)
+  }else if(source == "2"){
+    lvmean <- dplyr::filter(lavaan::parameterEstimates(fit), op == "~1" & lhs == lavaan::lavNames(fit, type = "lv")[[2]]) %>%
+      dplyr::select(., est)%>%
+      as.numeric(.)
+  }
+  return(lvmean)
+}
+
+#' @rdname es_helpers
+grouplvsd <- function(fit, source){
+  if(source == "1"){
+    lvsd <- dplyr::filter(lavaan::parameterEstimates(fit), op == "~~"& lhs == lavaan::lavNames(fit, type = "lv")[[1]] & rhs == lavaan::lavNames(fit, type = "lv")[[1]]) %>%
+      dplyr::select(., est) %>%
+      sqrt(.) %>%
+      as.numeric(.)
+  }else if(source == "2"){
+    lvsd <- dplyr::filter(lavaan::parameterEstimates(fit), op == "~~"& lhs == lavaan::lavNames(fit, type = "lv")[[2]] & rhs == lavaan::lavNames(fit, type = "lv")[[2]]) %>%
+      dplyr::select(., est) %>%
+      sqrt(.) %>%
+      as.numeric(.)
+  }
+  return(lvsd)
+}
+
---
+++ R/get_dydmacs.R
@@ -0,0 +1,89 @@
+#' Calculates dmacs difference in expected indicator scores for between dyad members
+#'
+#' @param dat data frame of indicators
+#' @param fit outputted dyadic cfa lavaan object
+#' @param nodewidth space between nodes during quadrature approximation (default = .01)
+#' @param lowerLV lowest latent variable value evaluated (default = -5)
+#' @param upperLV greatest latent variable value evaluated (default = 5)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' dat <-  DRES
+#' dvn <- dvn2(dat, x_order = "sip", x_stem = "sexsat", x_delim2=".", x_item_num="\\d+", distinguish_1="1", distinguish_2="2")
+#' config.script<- dyadCFA(dvn, lvname = "SexSat",  model = "configural")
+#' fit <- lavaan::cfa(config.script, data = dat, std.lv = FALSE,
+#' auto.fix.first= FALSE, meanstructure = TRUE)
+#' sexsat.dmacs <- get_dydmacs(dat, fit)
+get_dydmacs <- function(dat, fit, nodewidth = 0.01, lowerLV = -5, upperLV = 5){
+
+  #Get indicator names --> # of indicators
+  indnames <- lavaan::parameterestimates(fit) %>% #names of indicators
+    dplyr::filter(., op == "=~") %>%
+    dplyr::select(., rhs) %>%
+    unique(.)
+
+  p <- length(unique(indnames$rhs))/2
+
+  indnames1 <- indnames$rhs[1:p]
+  indnames2 <- indnames$rhs[(p+1):(p*2)]
+
+  lvnames <- lavaan::lavNames(fit, type = "lv")
+
+  #Group info
+  gnum <- 2
+  dyn <- lavaan::lavInspect(fit, what = "nobs")
+
+  #Use es_helpers to snatch necessary values from cfa model
+  load1 <- grouploads(fit, source = "1")
+  load2 <- grouploads(fit, source = "2")
+  loading1 <- load1$est
+  loading2 <- load2$est
+
+  int1 <- groupints(fit, source = "1")
+  int2 <- groupints(fit, source = "2")
+  intercept1 <- int1$est
+  intercept2 <- int2$est
+
+  stdev1 <- groupindsds(dat, source = 1)
+  stdev2 <- groupindsds(dat, source = 2)
+
+  fmean2 <- grouplvmean(fit, source = "2")
+
+  fsd2 <- grouplvsd(fit, source = "2")
+
+  N1 = dyn
+  N2 = dyn
+
+  # Define evaluation of latent variable
+  LV = seq(lowerLV,upperLV,nodewidth)
+
+  # Create empty matrices for future arrays, matrices, etc.
+  DiffExpScore = matrix(NA,length(LV),p)
+  pdfLV2 = matrix(NA,length(LV),1)
+  dmacsNumerator = matrix(NA,length(LV),p)
+  stdevpoolND = matrix(NA,p,1)
+  dmacs = matrix(NA,p,1)
+
+  # Calculate dmacs and signed dmacs
+  for(j in 1:p){
+    for(k in 1:length(LV)){
+      # Calculate difference in expected indicator scores between groups 1 and 2
+      DiffExpScore[k,j] <- (intercept1[j]-intercept2[j]) + (loading1[j]-loading2[j])*LV[k]
+      # probability density function for sample estimate of group 2 latent variable distribution
+      pdfLV2[k] = dnorm(LV[k], mean=fmean2, sd=fsd2)
+
+      # Multiply by latent variable distribution to calculate individual data point in numerator
+      dmacsNumerator[k,j] = DiffExpScore[k,j]*DiffExpScore[k,j]*pdfLV2[k]*nodewidth
+    }
+    # Calculate pooled SD based on Nye & Drasgow formula
+    stdevpoolND[j] = ((N1-1)*stdev1[j] + (N2-1)*stdev2[j])/(N1+N2-2)
+    # Sum across range of latent variable using quadrature to calculate numerator & divide by denominator
+    dmacs[j] <- sqrt(sum(dmacsNumerator[,j]))/stdevpoolND[j]
+  }
+  colnames(dmacs) = "dMACS"
+
+  return(dmacs=round(dmacs,4))
+
+}
