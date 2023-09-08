2427a7320ad9e47716dbae3e801dfdb28373cb5e
Omar Jordan Camanto <ocamanto@uwo.ca>
2023-09-08 12:18:17 -0400

    docs: improve documentation.

--- R/scriptBiDy.R
+++ R/scriptBiDy.R
@@ -1,5 +1,5 @@
 #' A Function That Writes, Saves, and Exports Syntax for
-#' Fitting Bifactor Dyadic (Bidy) Models
+#' Fitting Bifactor Dyadic (Bidy) models
 #'
 #' This function takes the outputted object from scrapeVarCross()
 #' and automatically writes, returns, and exports (.txt) lavaan() syntax
