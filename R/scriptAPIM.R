9a101169ba7368f2a3657f3af5c3f42113807325
Omar Jordan Camanto <ocamanto@uwo.ca>
2023-09-08 11:22:57 -0400

    docs: Add parenthetical "APIM"

--- R/scriptAPIM.R
+++ R/scriptAPIM.R
@@ -1,5 +1,5 @@
 #' A Function That Writes, Saves, and Exports Syntax for
-#' Fitting Latent Actor-Partner Interdependence Models
+#' Fitting Latent Actor-Partner Interdependence Models (APIM)
 #'
 #' This function takes the outputted object from scrapeVarCross()
 #' and automatically writes, returns, and exports (.txt) lavaan() syntax
