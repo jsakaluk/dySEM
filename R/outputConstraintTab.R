75cbad0089de2d1492b7688e6b67eadce1df688f
John Sakaluk <jksakaluk@gmail.com>
2025-01-07 15:46:55 -0500

    fix: markdown for outputter return values, and update pkgdown

--- R/outputConstraintTab.R
+++ R/outputConstraintTab.R
@@ -1,11 +1,11 @@
 #' Evaluate Invariance Equality Constraints in a specified Dyadic Invariance Model
 #'
-#' `outputConstraintTab()` is used to perform a score test for relaxing each 
+#' `outputConstraintTab()` is used to perform a score test for relaxing each
 #' invariance equality constraint between partners in a given dyadic SEM model.
 #'
 #' @param constrainFit A fitted `lavaan` model with dyadic invariance equality constraints
 #' @param filterSig A logical indicating whether to filter for significant constraints (default is `FALSE`)
-#' @param gtTab A logical input indicating whether to generate the output in `gt::gt()` table object format (`TRUE`). 
+#' @param gtTab A logical input indicating whether to generate the output in `gt::gt()` table object format (`TRUE`).
 #'  By default (`FALSE`), the output is generated in `tibble::tibble()` format.
 #'  Users can also apply the `writeTo` argument if they wish to export the `gt:gt()` table object.
 #' @param writeTo A character string specifying a directory path to where the `gt::gt()` table object should be saved.
@@ -17,14 +17,14 @@
 #'  The resulting base name will automatically be appended with a `.rtf` file extension.
 #'  `fileName` is only relevant if `gtTab = TRUE` and `writeTo` is specified.
 #'
-#' @return A list. More specifically, a [tibble()]---if `gtTab = FALSE` (default)---or `gt::gt()` object---if `gtTab = TRUE`---with 
+#' @return A `tibble::tibble()` if `gtTab = FALSE` (default), or `gt::gt()` object if `gtTab = TRUE`, with
 #' rows of equality constraints (now with readable `param` labels) and test statistic, *df*, and *p* for whether constraint worsens model fit.
-#' 
+#'
 #' @details
 #' * If `gtTab = TRUE` and `writeTo` is specified, then output will simultaneously be saved as a `.rtf` file to the user's specified directory.
 #' * If output file is successfully saved, a confirmation message will be printed to the console.
 #' * If a file with the same name already exists in the user's chosen directory, it will be overwritten.
-#' 
+#'
 #' @export
 #'
 #' @examples
@@ -40,14 +40,14 @@
 #'
 #' outputConstraintTab(sat.resids.mod, filterSig = FALSE,
 #' gtTab = TRUE, writeTo = tempdir(), fileName = "dCFA_Residual")
-#' 
+#'
 
-outputConstraintTab <- function(constrainFit, filterSig = FALSE, 
+outputConstraintTab <- function(constrainFit, filterSig = FALSE,
                                 gtTab = FALSE, writeTo = NULL, fileName = NULL){
-  
+
   #checking for valid directory path
   if (gtTab == TRUE && !is.null(writeTo)){
-    
+
     if (!is.character(writeTo)){
       stop("The `writeTo` argument must be a character string. \n Use `writeTo = '.'` to save output file(s) in the current working directory.")
     }
@@ -57,22 +57,22 @@ outputConstraintTab <- function(constrainFit, filterSig = FALSE,
     if (!is.null(fileName) && !is.character(fileName)){
       stop("The `fileName` argument must be a character string.")
     }
-    
+
   }
-  
-  
+
+
   #Extract score test for relaxing each equality constraint
   constrainFit.mi <- lavaan::lavTestScore(constrainFit)
   constrainFit.mi.uni <- constrainFit.mi$uni
   constrainFit.mi.uni <- constrainFit.mi.uni |>
     dplyr::rename(constraint = .data$op)
-  
+
   #Extract parameter table and create more informative parameter description
   constrainFit.PT <- lavaan::parTable(constrainFit)
   constrainFit.PT$param <- paste(constrainFit.PT$lhs, constrainFit.PT$op, constrainFit.PT$rhs, sep = " ")
   constrainFit.PT.reduce <- constrainFit.PT |>
     dplyr::select(.data$plabel, .data$param)
-  
+
   #Create subset of lhs labeled parameters
   constrainFit.PT.plabel.lhs <- constrainFit.PT.reduce |>
     #use dplyr filter to select for cases where plabel matches the values of lhs or rhs in int.fit.mi.uni
@@ -80,17 +80,17 @@ outputConstraintTab <- function(constrainFit, filterSig = FALSE,
     dplyr::rename(plabel1 = .data$plabel,
                   param1 = .data$param) |>
     dplyr::mutate(constraint = "==")
-  
+
   #Create subset of rhs labeled parameters
   constrainFit.PT.plabel.rhs <- constrainFit.PT.reduce |>
     #use dplyr filter to select for cases where plabel matches the values of lhs or rhs in int.fit.mi.uni
     dplyr::filter(.data$plabel %in% constrainFit.mi.uni$rhs) |>
     dplyr::rename(plabel2 = .data$plabel,
                   param2 = .data$param)
-  
+
   #cbind subsets together
   constrainFit.PT.merge <- cbind(constrainFit.PT.plabel.lhs, constrainFit.PT.plabel.rhs)
-  
+
   #carry over chi2, df, and p, and round
   constrainFit.PT.merge <- constrainFit.PT.merge |>
     dplyr::mutate(chi2 = constrainFit.mi.uni$X2,
@@ -101,32 +101,32 @@ outputConstraintTab <- function(constrainFit, filterSig = FALSE,
                                          pvalue < .001 ~ "***")) |>
     dplyr::select(-.data$plabel1, -.data$plabel2) |>
     dplyr::mutate_if(is.numeric, round, 3)
-  
+
   #filter for significance if filterSig is TRUE
   if(filterSig == TRUE){
     constrainFit.PT.merge <- constrainFit.PT.merge |>
       dplyr::filter(.data$pvalue < .05)
   }
-  
+
   if (gtTab == FALSE){
-    
+
     constrainFit.PT.merge <- constrainFit.PT.merge |>
       tibble::as_tibble()
-    
+
     return(constrainFit.PT.merge)
   }
-  
+
   else if (gtTab == TRUE){ #Create gt table
-    
-    
+
+
     #user specifies writeTo
     if (!is.null(writeTo)){
-      
+
       constrainFit.PT.merge <- constrainFit.PT.merge |>
         gt::gt()
-      
+
       if (is.null(fileName)){
-        gt::gtsave(constrainFit.PT.merge, 
+        gt::gtsave(constrainFit.PT.merge,
                    filename = "dySEM_table.rtf",
                    path = writeTo)
         message( #confirmation message
@@ -135,9 +135,9 @@ outputConstraintTab <- function(constrainFit, filterSig = FALSE,
             writeTo)
         )
       }
-      
+
       else if (!is.null(fileName)){
-        gt::gtsave(constrainFit.PT.merge, 
+        gt::gtsave(constrainFit.PT.merge,
                    filename = sprintf("%s.rtf",
                                       fileName),
                    path = writeTo)
@@ -148,19 +148,19 @@ outputConstraintTab <- function(constrainFit, filterSig = FALSE,
         )
       }
     }
-    
+
     #user does not specify writeTo
     else if (is.null(writeTo)){
-      
+
       constrainFit.PT.merge <- constrainFit.PT.merge |>
         gt::gt()
-      
+
     }
-    
+
     return(constrainFit.PT.merge)
 
   }
-  
+
 }
 
 
--- R/outputInvarCompTab.R
+++ R/outputInvarCompTab.R
@@ -1,10 +1,10 @@
-#' Compare Model Fit of Nested Dyadic Invariance Models 
-#' 
-#' `outputInvarCompTab()` is used to compare the model fit of nested dyadic invariance models 
+#' Compare Model Fit of Nested Dyadic Invariance Models
+#'
+#' `outputInvarCompTab()` is used to compare the model fit of nested dyadic invariance models
 #'  in order from most parsimonious (residual) to least parsimonious (configural)
 #'
 #' @param mods A list of nested `lavaan` dyadic invariance models, in the order of residual, intercept, loading, configural
-#' @param gtTab A logical input indicating whether to generate the output in `gt::gt()` table object format (`TRUE`). 
+#' @param gtTab A logical input indicating whether to generate the output in `gt::gt()` table object format (`TRUE`).
 #'  By default (`FALSE`), the output is generated in `tibble::tibble()` format.
 #'  Users can also apply the `writeTo` argument if they wish to export the `gt:gt()` table object.
 #' @param writeTo A character string specifying a directory path to where the `gt::gt()` table object should be saved.
@@ -16,17 +16,17 @@
 #'  The resulting base name will automatically be appended with a `.rtf` file extension.
 #'  `fileName` is only relevant if `gtTab = TRUE` and `writeTo` is specified.
 #'
-#' @return A list. More specifically, a [tibble()]---if `gtTab = FALSE` (default)---or `gt::gt()` object---if `gtTab = TRUE`---of model fit statistics for each model, as well as the difference in fit statistics between each model and the previous model
-#' 
+#' @return A `tibble::tibble()` if `gtTab = FALSE` (default), or `gt::gt()` object if `gtTab = TRUE`, of model fit statistics for each model, as well as the difference in fit statistics between each model and the previous model
+#'
 #' @details
 #' * If `gtTab = TRUE` and `writeTo` is specified, then output will simultaneously be saved as a `.rtf` file to the user's specified directory.
 #' * If output file is successfully saved, a confirmation message will be printed to the console.
 #' * If a file with the same name already exists in the user's chosen directory, it will be overwritten.
-#' 
+#'
 #' @export
 #'
 #' @examples
-#' 
+#'
 #' dvn <- scrapeVarCross(dat = commitmentQ, x_order = "spi",
 #' x_stem = "sat.g", x_delim1 = ".", x_delim2="_", distinguish_1="1", distinguish_2="2")
 #'
@@ -60,13 +60,13 @@
 #' gtTab = TRUE, writeTo = tempdir(), fileName = "dCFA_Invar")
 #'
 outputInvarCompTab <- function(mods,
-                               gtTab = FALSE, 
-                               writeTo = NULL, 
+                               gtTab = FALSE,
+                               writeTo = NULL,
                                fileName = NULL){
-  
+
   #checking for valid directory path
   if (gtTab == TRUE && !is.null(writeTo)){
-    
+
     if (!is.character(writeTo)){
       stop("The `writeTo` argument must be a character string. \n Use `writeTo = '.'` to save output file(s) in the current working directory.")
     }
@@ -76,22 +76,22 @@ outputInvarCompTab <- function(mods,
     if (!is.null(fileName) && !is.character(fileName)){
       stop("The `fileName` argument must be a character string.")
     }
-    
+
   }
-  
-  
+
+
   modfit <- t(as.data.frame(lavaan::fitmeasures(mods[[1]])))
-  
+
   for(i in 2:length(mods)){
     modfit_iter <- t(as.data.frame(lavaan::fitmeasures(mods[[i]])))
     modfit <- rbind(modfit, modfit_iter)
   }
-  
+
   modfit <- as.data.frame(modfit)
   modfit$mod <- c("residual", "intercept", "loading", "configural")
   modfit <- modfit |>
     dplyr::select(.data$mod, .data$chisq, .data$df, .data$pvalue, .data$aic, .data$bic, .data$rmsea, .data$cfi)
-  
+
   modcomp <- modfit |>
     #mutate _diff cols of each except p-value, where the column value for a particular row is is the difference of the previous and current row's value for that column
     dplyr::mutate(chisq_diff = c(NA, diff(.data$chisq)),
@@ -102,27 +102,27 @@ outputInvarCompTab <- function(mods,
                   rmsea_diff = c(NA, diff(.data$rmsea)),
                   cfi_diff = c(NA, diff(.data$cfi))) |>
     dplyr::mutate_if(is.numeric, round, 3)
-  
+
   rownames(modcomp) <- NULL
-  
+
   if (gtTab == FALSE){
-    
+
     modcomp <- modcomp |>
       tibble::as_tibble()
-    
+
     return(modcomp)
   }
-  
+
   else if (gtTab == TRUE){ #Create gt table
-  
+
     #user specifies writeTo
     if (!is.null(writeTo)){
-      
+
       modcomp <- modcomp |>
         gt::gt()
-      
+
       if (is.null(fileName)){
-        gt::gtsave(modcomp, 
+        gt::gtsave(modcomp,
                    filename = "dySEM_table.rtf",
                    path = writeTo)
         message( #confirmation message
@@ -131,9 +131,9 @@ outputInvarCompTab <- function(mods,
             writeTo)
         )
       }
-      
+
       else if (!is.null(fileName)){
-        gt::gtsave(modcomp, 
+        gt::gtsave(modcomp,
                    filename = sprintf("%s.rtf",
                                       fileName),
                    path = writeTo)
@@ -144,17 +144,17 @@ outputInvarCompTab <- function(mods,
         )
       }
     }
-    
+
     #user does not specify writeTo
     else if (is.null(writeTo)){
-      
+
       modcomp <- modcomp |>
         gt::gt()
-      
+
     }
-    
+
     return(modcomp)
-    
+
   }
-  
+
 }
